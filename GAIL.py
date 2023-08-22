b81d43e5aa9524546c38864ed869c7a73e815a84
Trusha Sonawane <73075228+fentywrap@users.noreply.github.com>
2023-08-22 16:23:18 +0530

    Add files via upload

---
+++ GAIL.py
@@ -0,0 +1,15 @@
+import plotly.graph_objects as go
+import pandas as pd
+df = pd.read_csv('GAIL.csv')
+fig = go.Figure([go.Scatter(x=df['Date'], y=df['High'])])
+
+fig.update_xaxes(ticks= "outside",
+                 ticklabelmode= "period",
+                 tickcolor= "black",
+                 ticklen=5,
+                 minor=dict(
+                     griddash='dot',
+                     gridcolor='black')
+                )
+fig.update_traces(line_color = 'dark blue')
+fig.show()
